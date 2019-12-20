66a247b14ffd36343e97defa4644fe23389aa340
Jaime Gensler <jaimegensler0@gmail.com>
2019-12-20 10:27:59 -0800

    Add test for Word#delete

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,9 +4,9 @@ require 'word'
 describe 'Word' do
     before :each do
         Word.clear
-        @word1 = Word.new({word: 'Coffee'}).send(:save)
-        @word2 = Word.new({word: 'Magnanimous'}).send(:save)
-        @word3 = Word.new({word: 'Program'}).send(:save)
+        @word1 = Word.new({word: 'Coffee'})
+        @word2 = Word.new({word: 'Magnanimous'})
+        @word3 = Word.new({word: 'Program'})
     end
 
     describe '#save' do
@@ -19,6 +19,12 @@ describe 'Word' do
         it 'updates the attributes of a Word' do
             @word1.update({word: 'Java'})
             expect(@word1.word).to eq 'Java'
+        end
+    end
+    describe '#delete' do
+        it 'removes a word from the list' do
+            @word1.delete
+            expect(Word.all).to eq ([@word2, @word3])
         end
     end
     describe '.all' do
