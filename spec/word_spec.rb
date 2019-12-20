6db4caf9523260d96d48838d576dc300a9201ea3
Jaime Gensler <jaimegensler0@gmail.com>
2019-12-20 09:47:40 -0800

    Fix minor bugs in tests

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,26 +3,32 @@ require 'word'
 
 describe 'Word' do
     before :each do
-        @word1 = Word.new({word: 'Coffee'})
-        @word2 = Word.new({word: 'Magnanimous'})
-        @word3 = Word.new({word: 'Program'})
+        Word.clear
+        @word1 = Word.new({word: 'Coffee'}).send(:save)
+        @word2 = Word.new({word: 'Magnanimous'}).send(:save)
+        @word3 = Word.new({word: 'Program'}).send(:save)
     end
 
     describe '#save' do
         it 'saves the word' do
-            word = Word.new('Resting').save
+            word = Word.new({word: 'Resting'}).send(:save)
             expect(Word.all).to include(word)
         end
     end
     describe '#initialize' do
         it 'instantiates a new object with unique ID' do
-            expect(word.word).to eq('Testing')
-            expect(word.id).to eq(4)
+            expect(@word1.word).to eq('Coffee')
         end
     end
     describe '.all' do
         it 'returns an array of all words' do
-            expect(Word.all).to eq ([@word1, @word2, @word3])
+            expect(Word.all).to eq([@word1, @word2, @word3])
+        end
+    end
+    describe '.clear' do
+        it 'clears all words' do
+            Word.clear
+            expect(Word.all).to eq([])
         end
     end
 end
