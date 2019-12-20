07d4ebe66da521b560be436613b9155a8ee3e155
Jaime Gensler <jaimegensler0@gmail.com>
2019-12-20 11:58:09 -0800

    Create initial tests for Definition

---
+++ spec/definition_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require 'definition'
+
+describe 'Definition' do
+    before :each do
+        #word_id 1 is "Program"
+        #word_id 2 is "Humongous"
+        Definition.clear
+        @def1 = Definition.new({definition: 'A set of related measures or activities with a particular long-term aim', part_of_speech: 'noun', word_id: 1})
+        @def2 = Definition.new({definition: 'Huge; enormous.', word_id: 2})
+        @def3 = Definition.new({definition: 'To provide (a computer or other machine) with coded instructions for the automatic performance of a task.', part_of_speech: 'verb', word_id: 1})
+    end
+
+    describe '#save' do
+        it 'saves the definition when initialized' do
+            def0 = Definition.new({definition: 'Resting'}) #save is run at the end of initialize
+            expect(Definition.all).to include(def0)
+        end
+    end
+    describe '.all' do
+        it 'returns an array of all definitions' do
+            expect(Definition.all).to eq([@def1, @def2, @def3])
+        end
+    end
+    describe '.clear' do
+        it 'clears all definitions' do
+            Definition.clear
+            expect(Definition.all).to eq([])
+        end
+    end
+end
