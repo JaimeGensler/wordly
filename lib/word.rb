e2dad7d73f133e3691a47a2a74c9509db450f54e
Jaime Gensler <jaimegensler0@gmail.com>
2019-12-20 09:48:16 -0800

    Add logic for tested methods (#save, .all, .clear)

--- lib/word.rb
+++ lib/word.rb
@@ -1,10 +1,23 @@
 class Word
     attr_accessor :word
     attr_reader :id
-    @@words = {}
-    @@current_id = 0
     def initialize (attributes)
         @word = attributes[:word]
         @id = (@@current_id += 1)
+    end
+
+    #class methods
+    @@words = {}
+    @@current_id = 0
+    def self.all
+        @@words.values
+    end
+    def self.clear
+        @@words = {}
+    end
+
+    private
+    def save
+        @@words[@id] = self
     end
 end
