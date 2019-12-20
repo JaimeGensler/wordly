f332df5eea84327c84b45f900bf9e24d30183aa3
Jaime Gensler <jaimegensler0@gmail.com>
2019-12-20 09:29:56 -0800

    Add test for #save, reformat tests

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,13 +3,21 @@ require 'word'
 
 describe 'Word' do
     before(:each) do
-        @word1 = Word.new({word: "Coffee"})
+        @word1 = Word.new({word: 'Coffee'})
+        @word2 = Word.new({word: 'Magnanimous'})
+        @word3 = Word.new({word: 'Program'})
     end
 
-    describe('#initialize') do
-        it("instantiates a new object with unique ID") do
-            expect(@word1.word).to(eq("Coffee"))
-            expect(@word1.id).to(eq(1))
+    describe '#save' do
+        it 'saves the word' do
+            word = Word.new('Resting').save
+            expect(Word.all).to include(word)
+        end
+    end
+    describe '#initialize' do
+        it 'instantiates a new object with unique ID' do
+            expect(word.word).to eq('Testing')
+            expect(word.id).to eq(4)
         end
     end
 end
