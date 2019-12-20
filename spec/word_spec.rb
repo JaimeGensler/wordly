06a1a60cf1bb4dffc89a000e822c84eb8f42ef9f
Jaime Gensler <jaimegensler0@gmail.com>
2019-12-20 09:38:11 -0800

    Add test for Word.all

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'word'
 
 describe 'Word' do
-    before(:each) do
+    before :each do
         @word1 = Word.new({word: 'Coffee'})
         @word2 = Word.new({word: 'Magnanimous'})
         @word3 = Word.new({word: 'Program'})
@@ -18,6 +18,11 @@ describe 'Word' do
         it 'instantiates a new object with unique ID' do
             expect(word.word).to eq('Testing')
             expect(word.id).to eq(4)
+        end
+    end
+    describe '.all' do
+        it 'returns an array of all words' do
+            expect(Word.all).to eq ([@word1, @word2, @word3])
         end
     end
 end
